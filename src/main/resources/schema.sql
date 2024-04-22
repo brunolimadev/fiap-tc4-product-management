98df1b897cfeb3628dcf583ca344a0e5e5d41b3a
Wiliam Nascimento <wilnasc990s3@gmail.com>
2024-04-22 20:06:39 -0300

    Adicionando estrutura inicial do projeto

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE products (
+
+    id INT,
+    description VARCHAR(255),
+    price NUMERIC,
+    storeQuantity INT
+
+);
---
+++ src/test/java/br/com/fiap/productmanagement/ProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.productmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
